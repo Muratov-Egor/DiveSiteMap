1ef4879b64436cd53a2103289e06f70c25d44d4c
Muratov Egor <egor_muratov@hotmail.com>
2025-03-02 21:54:19 +0100

    Улучшить стилизацию и разметку попапа дайв-сайта

--- src/app/components/DiveSitePopup.js
+++ src/app/components/DiveSitePopup.js
@@ -6,10 +6,12 @@ import { Popup } from "react-leaflet";
 const DiveSitePopup = ({site, onSelectSite}) => {
     return (
         <Popup>
-        <strong>{site.name}</strong>
-        <br />
-        {site.description || "Нет описания"}
-        <Button size="small" text="Подробнее" color="green" onClick={() => onSelectSite(site.id)} />
+          <div  className="flex flex-col items-center" >
+            <strong>{site.name}</strong>
+            <br />
+            <span className="mb-3">{site.description || "Нет описания"}</span>
+            <Button className="w-40" size="small" text="Подробнее" color="green" onClick={() => onSelectSite(site.id)} />
+        </div>
       </Popup>
     )
 }

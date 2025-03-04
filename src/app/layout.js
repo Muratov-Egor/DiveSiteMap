bf868f8ac78f001e76593f9eb296f572ac3af5ae
Muratov Egor <egor_muratov@hotmail.com>
2025-03-04 20:37:10 +0100

    Заменить шрифты Geist на Roboto для улучшения типографики

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,17 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import { DiveSitesProvider } from "./context/DiveSitesContext";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const robotoSans = Roboto({
+  variable: "--font-roboto-sans",
   subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
   subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 export const metadata = {
@@ -21,7 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased w-screen h-screen`}
+        className={`${robotoSans.variable} ${robotoMono.variable} antialiased w-screen h-screen`}
       >
         <DiveSitesProvider>
           {children}
